9545a6260bbe5cacc6c2856b21740bc8ec69255e
Felix Anducho <felix.anducho74@gmail.com>
2020-06-27 20:43:52 -0500

    Add measure list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const recipeBtn = document.querySelector('.recipeBtn')
 const recipeFeed = document.querySelector('.recipeFeed')
-const listFragment = document.createDocumentFragment()
+const recipeListFragment = document.createDocumentFragment()
+const measureListFragment = document.createDocumentFragment()
 const URL = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 
@@ -20,27 +21,62 @@ const formatIngredients = (ingredients) => {
     return ingredientsArr
 }
 
+const formatMeasures = (measure) => {
+    const measuresArr = 
+    [measure.strMeasure1, measure.strMeasure2,
+    measure.strMeasure3, measure.strMeasure4,
+    measure.strMeasure5, measure.strMeasure6,
+    measure.strMeasure7, measure.strMeasure8,
+    measure.strMeasure9, measure.strMeasure10,
+    measure.strMeasure11, measure.strMeasure12,
+    measure.strMeasure13, measure.strMeasure14,
+    measure.strMeasure15, measure.strMeasure16,
+    measure.strMeasure17, measure.strMeasure18,
+    measure.strMeasure19, measure.strMeasure20]
+
+    return measuresArr
+}
+
 const showRecipe = (recipe) => {
     const div = document.createElement('div')
     const title = document.createElement('h2')
     const description = document.createElement('p')
     const ingredientsList = document.createElement('ul')
+    const measureList = document.createElement('ul')
+
     const ingredientsArr = formatIngredients(recipe)
+    const measureArr = formatMeasures(recipe)
 
     ingredientsArr.forEach(element => {
         const ingredientSingle = document.createElement('li')
 
         ingredientSingle.textContent = element
-        listFragment.appendChild(ingredientSingle)
+        recipeListFragment.appendChild(ingredientSingle)
+
+        if(ingredientSingle.textContent === ''){
+            recipeListFragment.removeChild(ingredientSingle)
+        }
     });
 
-    ingredientsList.appendChild(listFragment)
+    measureArr.forEach(element => {
+        const measureSingle = document.createElement('li')
+
+        measureSingle.textContent = element
+        measureListFragment.appendChild(measureSingle)
+
+        if(measureSingle.textContent === ''){
+            measureListFragment.removeChild(measureSingle)
+        }
+    });
+
+    ingredientsList.appendChild(recipeListFragment)
+    measureList.appendChild(measureListFragment)
     title.textContent = recipe.strMeal
     description.textContent = recipe.strInstructions
     div.appendChild(title)
     div.appendChild(description)
     div.appendChild(ingredientsList)
-    div.classList.add('recipeElement')
+    div.appendChild(measureList)
 
     recipeFeed.append(div)
 }
